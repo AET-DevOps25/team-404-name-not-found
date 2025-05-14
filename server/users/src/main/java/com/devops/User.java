940413f520ece33c5717bcd38e346b33e1fcf730
Kristi <kristi@uni-bonn.de>
2025-05-14 22:19:34 +0200

    feat(users): add basic bearer auth driven users service

--- server/users/src/main/java/com/devops/User.java
+++ server/users/src/main/java/com/devops/User.java
@@ -3,15 +3,17 @@ package com.devops;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Person {
+@Entity
+@Table(name = "users")
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String name;
-    private int age;
+    @Column(unique = true)
+    private String username;
+    private String password;
 }
