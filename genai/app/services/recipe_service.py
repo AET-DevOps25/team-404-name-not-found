2775b52381f84691d13408c8c4aa0e44b6945e55
Franz <franz.kunze@tum.de>
2025-05-18 23:43:30 +0200

    [genai] mock OpenAI

--- genai/app/services/recipe_service.py
+++ genai/app/services/recipe_service.py
@@ -9,12 +9,13 @@ class RecipeService:
 
     def __init__(self):
         self.system_prompt = "Give a recipe that fits the ingredients."
-        self.llm = ChatOpenAI(model="gpt-4o", temperature=0.2)
+        # self.llm = ChatOpenAI(model="gpt-4o", temperature=0.2)
 
     async def get_recipe(self, ingredients: Ingredients) -> str:
         messages: List = [
             SystemMessage(content=self.system_prompt),
             HumanMessage(content=f"Ingredients: {ingredients.ingredients}"),
         ]
-        ai_msg = self.llm.invoke(messages)
-        return ai_msg.content
+        # ai_msg = self.llm.invoke(messages)
+        # return ai_msg.content
+        return f"Great Recipes to come from {ingredients.ingredients}"
