7452373e749b78873f074a32d9efbe8cd3d355ca
Kristi <kristi@uni-bonn.de>
2025-05-23 10:04:58 +0200

    fix(users): finally understand how JWT-auth works

--- server/users/src/main/java/com/devops/repositories/UserRepository.java
+++ server/users/src/main/java/com/devops/repositories/UserRepository.java
@@ -1,13 +1,11 @@
-package com.devops;
+package com.devops.repositories;
 
-import com.devops.User;
-
-import java.util.Optional;
-
+import com.devops.entities.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+    UserDetails findByEmail(String email);
 }
