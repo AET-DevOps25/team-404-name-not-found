f063dd57856a26fefa984d0599f41f991839ce86
Kristi <kristi@uni-bonn.de>
2025-05-28 15:10:39 +0200

    feat(users): add whoami for token verification

--- server/users/src/main/java/com/devops/controllers/AuthenticationController.java
+++ server/users/src/main/java/com/devops/controllers/AuthenticationController.java
@@ -6,6 +6,13 @@ import com.devops.entities.users.dtos.RegisterDTO;
 import com.devops.entities.users.User;
 import com.devops.infra.security.TokenService;
 import com.devops.repositories.UserRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
@@ -13,8 +20,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
