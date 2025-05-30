21b004e28ec32ca68046a5c5fd4c41b9314dc9a3
Marcel Kornegger <marcel.kornegger@tum.de>
2025-05-30 19:39:27 +0200

    Setup project for tailwind + shadcn ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { MantineProvider } from "@mantine/core";
-import { AuthProvider, useAuth } from './context/auth/AuthProvider';
-import PrivateRoute from './context/auth/PrivateRoute';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-
-// Root redirect component
-const RedirectFromRoot: React.FC = () => {
-    const { user } = useAuth();
-    return <Navigate to={user ? "/home" : "/login"} replace />;
-};
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/auth/AuthProvider';
+import Index from "./pages/Index.tsx";
 
 const App: React.FC = () => {
     return (
-        <MantineProvider>
-            <AuthProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<RedirectFromRoot />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route
-                            path="/home"
-                            element={
-                                <PrivateRoute>
-                                    <Dashboard />
-                                </PrivateRoute>
-                            }
-                        />
-                    </Routes>
-                </Router>
-            </AuthProvider>
-        </MantineProvider>
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Index/>} />
+                    <Route path="*" element={<Navigate to="/"/>} />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
     );
 };
 
