6d6c1eb2e006e3895582644a7cb11379cf7509f8
Kristi <kristi@uni-bonn.de>
2025-06-02 22:16:55 +0200

    feat(helm): define custom chart

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,6 @@
 
 helm install envoy oci://docker.io/envoyproxy/gateway-helm --version v1.4.0 -n envoy-gateway-system --create-namespace
 kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
+
+# this assumes you are running the script from project root
+helm install fridge ./fridge
