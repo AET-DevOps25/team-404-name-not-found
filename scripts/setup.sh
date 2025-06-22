f626d215335692c6c440a74b392ee01d3e7e06ec
Kristi <kristi@uni-bonn.de>
2025-06-22 11:24:40 +0200

    chore(ingress): add ingresses for the services

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,6 @@
 
 kubectl create ns team-404-name-not-found
 
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
-
 helm repo add jetstack https://charts.jetstack.io --force-update
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.2/cert-manager.crds.yaml
@@ -12,13 +10,8 @@ helm install \
   cert-manager jetstack/cert-manager \
   --version v1.17.2 \
   --create-namespace --namespace cert-manager \
-  --set config.apiVersion="controller.config.cert-manager.io/v1alpha1" \
-  --set config.kind="ControllerConfiguration" \
-  --set config.enableGatewayAPI=true
+  --set config.apiVersion="controller.config.cert-manager.io/v1alpha1"
 
-echo "#################################################################################"
-helm install envoy oci://docker.io/envoyproxy/gateway-helm --version v1.4.0 -n envoy-gateway-system --create-namespace
-kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
 echo "#################################################################################"
 
 # this assumes you are running the script from project root
