6c1a08f921b515ec4b1fb413dc182764439dbf4f
Kristi <kristi@uni-bonn.de>
2025-06-23 18:27:43 +0200

    test(ci): install cert-manager and test aet deployment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,13 +19,5 @@ echo "##########################################################################
 # this assumes you are running the script from project root
 helm upgrade --install fridge ./fridge --namespace team-404-name-not-found --create-namespace --atomic -f fridge/values.local.yaml
 
-echo "#################################################################################"
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/prometheus -n team-404-name-not-found -f ./fridge/prometheus.yml
-helm install grafana grafana/grafana -n team-404-name-not-found -f ./fridge/grafana.yml
-echo "#################################################################################"
-
 kubectl get pods -A
 echo "#################################################################################"
