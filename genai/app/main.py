45308bee060b562282c20a16faf305b7b4f83e62
Franz <franz.kunze@tum.de>
2025-07-02 00:38:03 +0200

    [genai] Add OpenApi spec descriptions

--- genai/app/main.py
+++ genai/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Body
 from prometheus_fastapi_instrumentator import Instrumentator
 from dotenv import load_dotenv
 
+from app.models.recipes import Recipes
 from app.services.recipe_service import RecipeService
 from app.models.ingredients import Ingredients
 
@@ -16,22 +17,58 @@ recipe_service = RecipeService()
 
 @app.get("/health")
 def health():
+    """
+    Health check endpoint.
+
+    If running returns a simple JSON response.
+
+    Returns:
+        dict: Key "status" set to "healthy"
+    """
     return {"status": "healthy"}
 
-@app.post("/recipe/matching")
-async def generate_recipe_matching(ingredients: Ingredients):
+
+@app.post("/recipe/matching",
+          response_model=Recipes,
+          summary="Generate one matching recipe",
+          description=(
+              "Generates one matching recipe that uses no more than the available ingredients. "
+          ))
+async def generate_recipe_matching(ingredients: Ingredients = Body(description="Available ingredients")):
     return await generate_recipes_matching(1, ingredients)
 
-@app.post("/recipe/matching/{num_recipes}")
-async def generate_recipes_matching(num_recipes: int, ingredients: Ingredients):
+
+@app.post("/recipe/matching/{num_recipes}",
+          response_model=Recipes,
+          summary="Generate number of matching recipes",
+          description=(
+              "Generates multiple matching recipes. "
+              "That are all different and use no more than the available ingredients."
+          ))
+async def generate_recipes_matching(num_recipes: int = Path(description="Number of recipes to return"),
+                                    ingredients: Ingredients = Body(description="Available ingredients")):
     recipes = await recipe_service.get_recipes_matching(num_recipes, ingredients)
     return recipes
 
-@app.post("/recipe/exploratory")
-async def generate_recipe_exploratory(ingredients: Ingredients):
+
+@app.post("/recipe/exploratory",
+          response_model=Recipes,
+          summary="Generate one exploratory recipe",
+          description=(
+              "An exploratory recipes can use more than the available ingredients as well as new ones. "
+          ))
+async def generate_recipe_exploratory(ingredients: Ingredients = Body(description="Available ingredients")):
     return await generate_recipes_exploratory(1, ingredients)
 
-@app.post("/recipe/exploratory/{num_recipes}")
-async def generate_recipes_exploratory(num_recipes: int, ingredients: Ingredients):
+
+@app.post("/recipe/exploratory/{num_recipes}",
+          response_model=Recipes,
+          summary="Generate number of exploratory recipes",
+          description=(
+              "An exploratory recipes can use more than the available ingredients as well as new ones. "
+              "All returned recipes are different. "
+          ))
+async def generate_recipes_exploratory(num_recipes: int = Path(description="Number of recipes to return"),
+                                       ingredients: Ingredients = Body(description="Available ingredients")):
     recipes = await recipe_service.get_recipes_exploratory(num_recipes, ingredients)
     return recipes
